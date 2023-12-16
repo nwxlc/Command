4bfdd8795753013c39c6e18148f6d6d5fd24f1d2
nwxlc <jeffkiller04@mail.ru>
2023-12-16 13:28:27 +0200

    Created

---
+++ Command/Program.cs
@@ -0,0 +1,18 @@
+﻿using Command;
+using Command.Conditioner;
+using Command.Door;
+using Command.Light;
+
+Remote remote = new Remote();
+Light light = new Light();
+Conditioner conditioner = new Conditioner();
+Door door = new Door();
+
+remote.AddCommand(0, new OpenDoorCommand(door), new CloseDoorCommand(door));
+remote.Execute(0);
+
+remote.AddCommand(1, new OnLight(light), new OffLight(light));
+remote.Execute(1);
+
+remote.Undo();
+remote.Undo();
